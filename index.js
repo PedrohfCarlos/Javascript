f0fbb1a62f9576f983343692d7c699c2b7ce465b
PedrohfCarlos <pedrohenriquefilgueiracarlos2@gmail.com>
2024-12-27 02:01:23 -0300

    nova Alteração

---
+++ index.js
@@ -0,0 +1,12 @@
+const helloworld = () => {
+  return "hello World";
+};
+const obj = {
+  prop: {
+    nome: "Manual do Dev",
+    info: {
+      youtube: "/manual do dev",
+      url: { value: "" },
+    },
+  },
+};
