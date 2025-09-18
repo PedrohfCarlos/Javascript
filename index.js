062910d760e4c6312951bc32d9fc5e1ec4f90262
PedrohfCarlos <pedrohenriquefilgueiracarlos2@gmail.com>
2025-09-18 00:17:48 -0300

    correção

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const nomeMusica = document.getElementById(`nome-da-musica`);
-const audio = documebnt.getElementById("audio");
+const audio = document.getElementById("audio");
 nomeMusica.innerText = "Minha Musica Favorita";
-musicas.play();
+audio.play();
