c5e334b1c263a6fa764a2081167e2412a0f6eba7
PedrohfCarlos <pedrohenriquefilgueiracarlos2@gmail.com>
2025-09-23 19:03:45 -0300

    nova atualização do JS

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const nomeMusica = document.getElementById(`nome-da-musica`);
 const audio = document.getElementById("audio");
 nomeMusica.innerText = "Minha Musica Favorita";
 audio.play();
+audio.pause();
